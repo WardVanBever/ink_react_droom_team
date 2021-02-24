5e1eede20213fc98249c80af5fe0e0d8e7fbc0de
ward <ward.van.bever@student.howest.be>
2021-02-24 09:04:32 +0100

    initial commit

---
+++ cli.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+'use strict';
+const React = require('react');
+const importJsx = require('import-jsx');
+const {render} = require('ink');
+const meow = require('meow');
+
+const ui = importJsx('./ui');
+
+const cli = meow(`
+	Usage
+	  $ ink_react_team
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ ink_react_team --name=Jane
+	  Hello, Jane
+`);
+
+render(React.createElement(ui, cli.flags));
