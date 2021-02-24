0e1a777633886e2ff92779251b52958ca8d9913c
kajdemuynck <kaj.de.muynck@student.howest.be>
2021-02-24 11:23:45 +0100

    Second draft

--- ui.js
+++ ui.js
@@ -6,17 +6,12 @@ const { useState } = require('react');
 const Snake = importJsx("./components/Snake");
 
 const App = ({name = 'Stranger'}) => {
-	const [screen, setScreen] = useState({w: 40, h: 30});
-	const [pos, setPos] = useState({x: 0, y: 0});
-	const [size, setSize] = useState(5);
-	const [snake, setSnake] = useState([]);
+	const [windowSize, setWindowSize] = useState({w: 20, h: 10});
+	const [snake, setSnake] = useState([[1,1], [1,2], [1,3], [2,3], [2,4]]);
 
 	return (
 	<>
-		<Text>
-			Hello, <Text color="green">{name}</Text>
-		</Text>
-		<Snake pos={pos} size={size} screen={screen} />
+		<Snake snake={snake} windowSize={windowSize} />
 	</>
 	);
 };
